1fb226bcbbfac5578a22b7a9d3d45610152028a9
Cinosargo22 <cinosargo22@gmail.com>
2022-06-18 19:44:59 -0700

    fourth commit

--- file.js
+++ file.js
@@ -1,7 +1,7 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
@@ -10,10 +10,9 @@ function writePassword() {
   
   }
 
-  // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Character sets
+
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specialCharacters = ["!", "#", "$", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@"];
 var lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -21,25 +20,23 @@ var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "
 
 function generatePassword () {
 
-//local variables
+
 var characterSet = [];
 var generatedPassword = "";
-
 var lenght;
-
 var useNumbers;
 var useSpecial;
 var useLower;
 var useUpper;
 
 
-lenght = window.prompt( "Please enter the lenght you would like for your password.", "");
+lenght = window.prompt( "How long do you want your password to be?", "");
 
 if ( lenght >= 8 && lenght <= 128 ) {
-useNumbers = window.confirm( "Would you like to use Numbers?\n(OK-Yes Cancel-No)" );
-useSpecial = window.confirm( "Would you like to use Special Characters?\n(OK-Yes Cancel-No)" );
-useLower = window.confirm( "Would you like to use Lower Case Letters?\n(OK-Yes Cancel-No)" );
-useUpper = window.confirm( "Would you like to use Upper Case Letters?\n(OK-Yes Cancel-No)" );
+useNumbers = window.confirm( "Use Numbers?\n(OK-Yes Cancel-No)" );
+useSpecial = window.confirm( "Use Special Characters?\n(OK-Yes Cancel-No)" );
+useLower = window.confirm( "Use Lower Case Letters?\n(OK-Yes Cancel-No)" );
+useUpper = window.confirm( "Use Upper Case Letters?\n(OK-Yes Cancel-No)" );
 }
 }
 
@@ -57,4 +54,4 @@ if ( useNumbers || useSpecial || useLower || useUpper ) {
     if ( useUpper ) {
         characterSet = characterSet.concat( upperCaseLetters );
     }
-
+}
