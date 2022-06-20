b8802c84c1bbd298abe7e3af3e47943ecbb5beba
Cinosargo22 <cinosargo22@gmail.com>
2022-06-19 20:35:39 -0700

    fifth commit

--- file.js
+++ file.js
@@ -1,57 +1,66 @@
-
 var generateBtn = document.querySelector("#generate");
 
-
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  
-  }
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
 
 generateBtn.addEventListener("click", writePassword);
 
-
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var specialCharacters = ["!", "#", "$", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@"];
-var lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialCharacters = ["!", "#", "$", "&","(",")","*","+","-",".","/",":",";","<","=",">","?","@",];
+var lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+var upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+include ="";
 
-function generatePassword () {
+function generatePassword() {
+  var chaSet = []
+  var newPassword = "";
+  var lenght;
+  var useNumbers;
+  var useSpecial;
+  var useLower;
+  var useUpper;
+  //Defining the function & declaring variables
+  lenght = window.prompt("How long do you want your password to be?", "");
+  // Prompt window to pop up
 
-
-var characterSet = [];
-var generatedPassword = "";
-var lenght;
-var useNumbers;
-var useSpecial;
-var useLower;
-var useUpper;
-
-
-lenght = window.prompt( "How long do you want your password to be?", "");
-
-if ( lenght >= 8 && lenght <= 128 ) {
-useNumbers = window.confirm( "Use Numbers?\n(OK-Yes Cancel-No)" );
-useSpecial = window.confirm( "Use Special Characters?\n(OK-Yes Cancel-No)" );
-useLower = window.confirm( "Use Lower Case Letters?\n(OK-Yes Cancel-No)" );
-useUpper = window.confirm( "Use Upper Case Letters?\n(OK-Yes Cancel-No)" );
+  if (lenght >= 8 && lenght <= 128) {
+    useNumbers = window.confirm("Use Numbers?\n(OK-Yes Cance-No)");
+    console.log (useNumbers)
+    useSpecial = window.confirm("Use Special Characters?\n(OK-Yes Cancel-No)");
+    console.log (useSpecial)
+    useLower = window.confirm("Use Lower Case Letters?\n(OK-Yes Cancel-No)");
+    console.log (useLower)
+    useUpper = window.confirm("Use Upper Case Letters?\n(OK-Yes Cancel-No)");
+    console.log (useUpper)
+  }
+ if(useNumbers.true){
+    include += numbers;
+ }
+if(useNumbers.false){
+    include
 }
-}
-
-
-if ( useNumbers || useSpecial || useLower || useUpper ) {
-    if ( useNumbers ) {
-        characterSet = characterSet.concat( numbers );
-    }
-    if ( useSpecial ) {
-        characterSet = characterSet.concat( specialCharacters );
-    }
-    if ( useLower ) {
-        characterSet = characterSet.concat( lowerCaseLetters );
-    }
-    if ( useUpper ) {
-        characterSet = characterSet.concat( upperCaseLetters );
-    }
+ if(useSpecial.true){
+    include += specialCharacters;
+ }
+ if(useSpecial.false){
+    include
+ }
+ if(useLower.true){
+    include += lowerCaseLetters;
+ }
+ if(useLower.false){
+    include
+ }
+ if(useUpper.true){
+    include += upperCaseLetters;
+ }
+ if(useUpper.false){
+    include
+ }
+return "password"
+    
 }
